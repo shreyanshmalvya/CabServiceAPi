b04d5838be07dc41dbadb281b5df5e82e54fbe33
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-26 02:40:39 +0530

    cors function enalbled

--- server/app.js
+++ server/app.js
@@ -30,6 +30,13 @@ const corsOpts = {
     ],
   };
 app.use(cors(corsOpts));
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+})
 
 // we use routes to directly route our data 
 
