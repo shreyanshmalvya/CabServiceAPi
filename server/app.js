8f5abe169fab496539f636be732bc078a9b81730
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-26 02:50:20 +0530

    updated cors function

--- server/app.js
+++ server/app.js
@@ -3,29 +3,17 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
-const cors = require('cors')
 
 //handling CORS errors, access-control-allow- (origin, headers, methods)
-const corsOpts = {
-    origin: '*',
-  
-    methods: [
-      'GET',
-      'POST',
-    ],
-  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
-app.use(cors(corsOpts));
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+app.use((req,res,next) =>{
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    //for options request
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'GET, DELETE, POST')
+    }
     next();
-})
+});
 
 //import routes
 const productRoutes = require('./api/routes/product');
