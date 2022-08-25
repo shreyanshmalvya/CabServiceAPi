2b0ae4f087fcd009363fbaed8c5e8e76db1090bb
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-26 02:58:51 +0530

    updated url

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser')
 
 //handling CORS errors, access-control-allow- (origin, headers, methods)
 app.use((req,res,next) =>{
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://cabservice-v1.web.app/');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     //for options request
     if(req.method === 'OPTIONS'){
